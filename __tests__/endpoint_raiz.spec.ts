6f7cd9872d3366faea4050a227e22aa09cdfce52
phetthross <pedro.obreque.r@gmail.com>
2024-12-06 15:18:36 -0300

    archivo base de pruebas

---
+++ __tests__/endpoint_raiz.spec.ts
@@ -0,0 +1,17 @@
+import { response } from 'express';
+import app from '../src/server.js';
+import request from 'supertest';
+
+describe('Bateria de test del servidor', () => {
+
+    test('servidor en endpoint /', async () => {
+        return await request(app)
+            .get('/')
+            .expect(200)
+            .expect('Content-Type', /text/)
+            .then(response => {
+                expect(response.text).toBe('Hola mundo al usuario spensiones');
+            });
+    });
+
+});
